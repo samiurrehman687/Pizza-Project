6b7a715911dfcc4e1adf75da93760775fd78d2dc
Article Django Site <samiurrehman38631@gmail.com>
2026-02-25 10:57:20 +0500

    Update urls.py

--- pizzanest/urls.py
+++ pizzanest/urls.py
@@ -4,7 +4,6 @@ from django.urls import path ,include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path("", include('account.urls')),
     path('', include('customer.urls')),
