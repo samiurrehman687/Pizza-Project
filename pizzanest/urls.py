92d6306f1bab1ce48b8c46c6c3a9fd134ee1e6ec
Article Django Site <samiurrehman38631@gmail.com>
2026-05-23 16:21:53 +0500

    url update

--- pizzanest/urls.py
+++ pizzanest/urls.py
@@ -4,9 +4,9 @@ from django.urls import path ,include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include('account.urls')),
-    path('', include('customer.urls')),
+    path('slice786zaika/', admin.site.urls),
+    path("account/", include('account.urls')),
+    path('customer/', include('customer.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
